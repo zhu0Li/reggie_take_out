c9c58965e7c7730b5443a65d32734e942703f423
zhu0Li <522192251@qq.com>
2023-03-11 20:02:30 +0800

    对应员工表添加实体类Employee

---
+++ src/main/java/com/reggie/entity/Employee.java
@@ -0,0 +1,56 @@
+package com.reggie.entity;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * 员工实体类
+ */
+@Data //自动整合Setter getter tostring 方法。
+public class Employee implements Serializable { //Serializable 实现序列化
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+
+
+    //菜品id
+    private Long dishId;
+
+
+    //口味名称
+    private String name;
+
+
+    //口味数据list
+    private String value;
+
+    /**
+     * fill: 自动填充策略
+     * DEFAULT	默认不处理
+     * INSERT	插入填充字段
+     * UPDATE	更新填充字段
+     * INSERT_UPDATE	插入和更新填充字段
+     */
+    @TableField(fill = FieldFill.INSERT)
+    private LocalDateTime createTime;
+
+
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private LocalDateTime updateTime;
+
+
+    @TableField(fill = FieldFill.INSERT)
+    private Long createUser;
+
+
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Long updateUser;
+
+
+    //是否删除
+    private Integer isDeleted;
+}
