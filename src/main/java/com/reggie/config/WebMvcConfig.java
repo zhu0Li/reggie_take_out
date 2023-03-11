cd448eb4a887a1c67b18c6050b478cb2276384a8
zhu0Li <522192251@qq.com>
2023-03-11 19:28:41 +0800

    设置静态资源映射，说明在WebMvcConfig当中

---
+++ src/main/java/com/reggie/config/WebMvcConfig.java
@@ -0,0 +1,25 @@
+package com.reggie.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Slf4j
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurationSupport {
+    /**
+     * 设置静态资源映射；
+     * 因为没有将前端代码放入静态static文件夹中，所以必须设置映射到resources/backend 和 front中
+     * @param registry
+     */
+
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        log.info("开始静态资源映射");
+        registry.addResourceHandler("/backend/**")
+                .addResourceLocations("classpath:/backend/");
+        registry.addResourceHandler("/front/**")
+                .addResourceLocations("classpath:/front/");
+    }
+}
