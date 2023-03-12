6aaac212b56f1f1503009cd26928d9e68cd1778b
zhu0Li <522192251@qq.com>
2023-03-12 16:22:02 +0800

    配置MP分页插件

---
+++ src/main/java/com/reggie/config/MybatisPlusConfig.java
@@ -0,0 +1,21 @@
+package com.reggie.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 配置MP分页插件
+ * 通过拦截器方式
+ */
+@Configuration
+public class MybatisPlusConfig {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor(){
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        return mybatisPlusInterceptor;
+    }
+}
