86bfdc67af98643d734f33a2d9f06530b6431914
zhu0Li <522192251@qq.com>
2023-03-13 15:01:40 +0800

    添加Dish和Setmeal实体及各种接口实现类

---
+++ src/main/java/com/reggie/service/impl/DishServiceImpl.java
@@ -0,0 +1,11 @@
+package com.reggie.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.reggie.entity.Dish;
+import com.reggie.mapper.DishMapper;
+import com.reggie.service.DishService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DishServiceImpl extends ServiceImpl<DishMapper, Dish> implements DishService {
+}
