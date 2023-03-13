7851e394a117499eca097dabe53011444a88d784
zhu0Li <522192251@qq.com>
2023-03-13 14:53:18 +0800

    初步实现删除分类功能

--- src/main/java/com/reggie/controller/CategoryController.java
+++ src/main/java/com/reggie/controller/CategoryController.java
@@ -1,15 +1,17 @@
 package com.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.reggie.common.R;
 import com.reggie.entity.Category;
+import com.reggie.entity.Employee;
 import com.reggie.service.CategoryService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.DigestUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -17,6 +19,24 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoryController {
     @Autowired
     private CategoryService categoryService;
+
+    /**
+     * 分页查询
+     * 使用封装好的page方法
+     */
+    @GetMapping("/page")
+    public R<Page> page(int page, int pageSize){
+        Page<Category> pageInfo = new Page<Category>(page, pageSize);
+
+        //构造分页构造器
+        LambdaQueryWrapper<Category> queryWrapper = new LambdaQueryWrapper<>();
+
+        //添加排序条件
+        queryWrapper.orderByAsc(Category::getType);
+        queryWrapper.orderByAsc(Category::getSort);
+        categoryService.page(pageInfo,queryWrapper);
+        return R.success(pageInfo);
+    }
 
     /**
      * 新增分类
@@ -30,5 +50,19 @@ public class CategoryController {
         categoryService.save(category);
 
         return R.success("分类添加成功");
+    }
+
+    /**
+     * 根据id来删除分类
+     * @param id
+     * @return
+     */
+    @DeleteMapping
+    public R<String> delete(Long id){
+        log.info("删除分类，id：{}",id);
+
+
+        categoryService.removeById(id);
+        return R.success("分类信息删除成功");
     }
 }
