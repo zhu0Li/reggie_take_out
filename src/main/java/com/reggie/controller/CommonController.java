38bdd1115bfbd4d167d0149a11539dee5dacc0aa
zhu0Li <522192251@qq.com>
2023-03-13 17:09:51 +0800

    实现文件上传功能，即通过页面上传图片，并存储到后端中

--- src/main/java/com/reggie/controller/CommonController.java
+++ src/main/java/com/reggie/controller/CommonController.java
@@ -2,10 +2,15 @@ package com.reggie.controller;
 
 import com.reggie.common.R;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
 
 /**
  * 文件的上传和下载
@@ -14,6 +19,10 @@ import org.springframework.web.multipart.MultipartFile;
 @RequestMapping("/common")
 @Slf4j
 public class CommonController {
+
+    //定义变量传入application.yml中设置的地址
+    @Value("${riggie.path}")
+    private String basePath;
 
     /**
      * 文件上传
@@ -24,6 +33,30 @@ public class CommonController {
     public R<String> upload(MultipartFile file){//MultipartFile后的变量名需要和网页中name中的名字相同
         //file是一个临时文件，需要转存到指定位置，否则本次请求完成后临时文件会删除
         log.info(file.toString());
-        return null;
+
+        //获取原始文件名
+        String originalFilename = file.getOriginalFilename();
+        //获取原始文件后缀
+        String suffix = originalFilename.substring(originalFilename.lastIndexOf("."));
+
+        //防止文件覆盖，使用UUID重新生成文件名，防止文件名字重复造成覆盖，并加上原始文件后缀
+        String fileName = UUID.randomUUID().toString() + suffix;
+
+        //创建一个目录对象
+        File dir = new File(basePath);
+        //判断当前目录是否存在
+        if(!dir.exists()){
+            //目录不存在，需要创建
+            dir.mkdir();
+        }
+
+        try {
+            //将临时文件转存到指定位置
+            file.transferTo(new File(basePath + fileName));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        //最后需要给页面返回文件，用于新增菜品
+        return R.success(fileName);
     }
 }
---
+++ src/test/java/com/reggie/test/Upload.java
@@ -0,0 +1,11 @@
+package com.reggie.test;
+
+import org.junit.jupiter.api.Test;
+
+public class Upload {
+    @Test
+    public  void test1(){
+        String fileName = "ad123.jpg";
+
+    }
+}
