7e252a6773c92f8b1945873624c57e86024cf28c
zhu0Li <522192251@qq.com>
2023-03-13 11:39:50 +0800

    实现分类添加功能

--- src/main/java/com/reggie/controller/CategoryController.java
+++ src/main/java/com/reggie/controller/CategoryController.java
@@ -1,8 +1,13 @@
 package com.reggie.controller;
 
+import com.reggie.common.R;
+import com.reggie.entity.Category;
 import com.reggie.service.CategoryService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.DigestUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +18,17 @@ public class CategoryController {
     @Autowired
     private CategoryService categoryService;
 
+    /**
+     * 新增分类
+     * 因为已经添加全局异常处理器，所以如果重复添加分类会跳转到异常处理
+     * @param category
+     * @return
+     */
+    @PostMapping
+    public R<String> save(@RequestBody Category category){
+        log.info("新增分类，分类信息{}", category.toString());
+        categoryService.save(category);
 
+        return R.success("分类添加成功");
+    }
 }
