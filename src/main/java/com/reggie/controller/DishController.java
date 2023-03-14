461e0326b656c2279cb31d69364cce9ef89d1f86
zhu0Li <522192251@qq.com>
2023-03-14 11:18:13 +0800

    实现菜品显示分页功能，包含显示图片以及分类名称

--- src/main/java/com/reggie/controller/DishController.java
+++ src/main/java/com/reggie/controller/DishController.java
@@ -1,16 +1,24 @@
 package com.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.reggie.common.R;
 import com.reggie.dto.DishDto;
+import com.reggie.entity.Category;
 import com.reggie.entity.Dish;
+import com.reggie.entity.Employee;
+import com.reggie.service.CategoryService;
 import com.reggie.service.DishFlavorService;
 import com.reggie.service.DishService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 菜品管理
@@ -26,6 +34,9 @@ public class DishController {
     @Autowired
     private DishService dishService;
 
+    @Autowired
+    private CategoryService categoryService;
+
     /**
      * 新增菜品
      * @param dishDto
@@ -39,5 +50,55 @@ public class DishController {
         return R.success("新增菜品成功");
     }
 
+    /**
+     * 菜品信息分页
+     * @param page
+     * @param pageSize
+     * @param name
+     * @return
+     */
+    @GetMapping("/page")
+    public R<Page> page(Integer page, Integer pageSize, String name){
+        log.info("page = {}, pageSize = {}, name = {}", page, pageSize, name);
 
+        //构造分页构造器
+        Page<Dish> pageInfo = new Page<>(page, pageSize);
+        //构造包含菜品分类名的page对象
+        Page<DishDto> dishDtoPage = new Page<>();
+
+        //构造条件构造器，name
+        LambdaQueryWrapper<Dish> queryWrapper = new LambdaQueryWrapper<>();
+        //添加过滤条件
+        queryWrapper.like(name!=null, Dish::getName,name);
+        //添加排序条件
+        queryWrapper.orderByDesc(Dish::getUpdateTime);
+
+        //执行查询
+        dishService.page(pageInfo,queryWrapper); //因为在.page方法中已经实现返回，所以不需要返回查询结果
+
+        //对象拷贝
+        BeanUtils.copyProperties(pageInfo,dishDtoPage,"records");
+
+        List<Dish> recoders = pageInfo.getRecords();
+
+        //stream流可以用foreach方法替代
+        List<DishDto> list = recoders.stream().map((item) -> {
+            DishDto dishDto = new DishDto();
+
+            BeanUtils.copyProperties(item, dishDto);
+
+            Long categoryId = item.getCategoryId();
+            //根据id查询分类对象
+            Category category = categoryService.getById(categoryId);
+            if(category!=null) {
+                String categoryName = category.getName();
+                dishDto.setCategoryName(categoryName);
+            }
+            return dishDto;
+        }).collect(Collectors.toList());
+
+        dishDtoPage.setRecords(list);
+
+        return R.success(dishDtoPage);
+    }
 }
