03b95d572b72174559c3687097f985b219f40124
zhu0Li <522192251@qq.com>
2023-03-15 22:29:49 +0800

    实现发送验证码方法

--- src/main/java/com/reggie/controller/UserController.java
+++ src/main/java/com/reggie/controller/UserController.java
@@ -1,10 +1,19 @@
 package com.reggie.controller;
+import com.reggie.utils.MailUtils;
+import org.apache.commons.lang.StringUtils;
 
+import com.reggie.common.R;
+import com.reggie.entity.User;
 import com.reggie.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.mail.MessagingException;
+import javax.servlet.http.HttpSession;
 
 @RestController
 @RequestMapping("/user")
@@ -13,4 +22,27 @@ public class UserController{
 
     @Autowired
     private UserService userService;
+
+    /**
+     * 发送手机短信验证码
+     * @param user
+     * @return
+     */
+    @PostMapping("/sendMsg")
+    public R<String> sendMsg(@RequestBody User user, HttpSession session) throws MessagingException {
+        // 获取手机号
+        String phone = user.getPhone();
+
+        if(StringUtils.isNotEmpty(phone)){
+            //生成随机验证码
+            String code = MailUtils.achieveCode();
+            //这里的phone其实就是邮箱，code是我们生成的验证码
+            MailUtils.sendTestMail(phone, code);
+            log.info("验证码为：{}",code);
+            //验证码存session，方便后面拿来对比进行登录
+//            session.setAttribute(phone, code);
+            return R.success("验证码发送成功");
+        }
+        return R.error("验证码发送失败");
+    }
 }
