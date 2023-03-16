be2db381b58dc4aa06acf01973e15949f3d0b51a
zhu0Li <522192251@qq.com>
2023-03-16 09:56:30 +0800

    构建shoppingcart的整体框架

---
+++ src/main/java/com/reggie/controller/ShoppingCartController.java
@@ -0,0 +1,27 @@
+package com.reggie.controller;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.reggie.common.BaseContext;
+import com.reggie.common.R;
+import com.reggie.entity.ShoppingCart;
+import com.reggie.service.ShoppingCartService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * 购物车
+ */
+@Slf4j
+@RestController
+@RequestMapping("/shoppingCart")
+public class ShoppingCartController {
+
+    @Autowired
+    private ShoppingCartService shoppingCartService;
+
+
+}
