08a06d7615e3d72a5bcce7f99a875bc8f6cb8032
zhu0Li <522192251@qq.com>
2023-03-16 11:25:06 +0800

    构建订单相关的基础类

---
+++ src/main/java/com/reggie/service/impl/OrderServiceImpl.java
@@ -0,0 +1,48 @@
+package com.reggie.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.IdWorker;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.reggie.common.BaseContext;
+import com.reggie.common.CustomException;
+import com.reggie.entity.*;
+import com.reggie.mapper.OrderMapper;
+import com.reggie.service.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+@Service
+@Slf4j
+public class OrderServiceImpl extends ServiceImpl<OrderMapper, Orders> implements OrderService {
+
+    @Autowired
+    private ShoppingCartService shoppingCartService;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private AddressBookService addressBookService;
+
+    @Autowired
+    private OrderDetailService orderDetailService;
+
+    /**
+     * 用户下单
+     * @param orders
+     */
+    @Override
+    public void submit(Orders orders) {
+
+
+
+    }
+}
