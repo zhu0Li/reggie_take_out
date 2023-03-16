4c69d858a388524e44f83acaa2edbb2b3ad232e7
zhu0Li <522192251@qq.com>
2023-03-16 14:52:18 +0800

    实现用户下单功能

--- src/main/java/com/reggie/service/impl/OrderServiceImpl.java
+++ src/main/java/com/reggie/service/impl/OrderServiceImpl.java
@@ -40,9 +40,80 @@ public class OrderServiceImpl extends ServiceImpl<OrderMapper, Orders> implement
      * @param orders
      */
     @Override
+    @Transactional
     public void submit(Orders orders) {
+        //获得当前用户id
+        Long userId = BaseContext.getCurrentId();
+
+        //查询当前用户的购物车数据
+        LambdaQueryWrapper<ShoppingCart> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(ShoppingCart::getUserId, userId);
+        List<ShoppingCart> shoppingCarts = shoppingCartService.list(queryWrapper);
+
+        if (shoppingCarts==null||shoppingCarts.size()==0){
+            throw new CustomException("购物车为空，不能下单");
+        }
+
+        //查询用户信息
+        User user = userService.getById(userId);
+
+        //查询地址信息
+        Long addressBookId = orders.getAddressBookId();
+        AddressBook addressBook = addressBookService.getById(addressBookId);
+        if (addressBook==null){
+            throw new CustomException("用户地址信息有误，不能下单");
+        }
+
+        //使用mp自带的包生成订单号
+        long orderId = IdWorker.getId();
+
+        //AtomicInteger 同步：多线程并发访问共享数据时，保证共享数据再同一时刻只被一个或一些线程使用。
+        //保证线程安全
+        AtomicInteger amount = new AtomicInteger(0);
+
+        //1、得到订单明细  2、计算总金额
+        List<OrderDetail> orderDetails = shoppingCarts.stream().map((item)->{
+            OrderDetail orderDetail = new OrderDetail();
+            orderDetail.setOrderId(orderId);
+            orderDetail.setNumber(item.getNumber());
+            orderDetail.setDishFlavor(item.getDishFlavor());
+            orderDetail.setDishId(item.getDishId());
+            orderDetail.setSetmealId(item.getSetmealId());
+            orderDetail.setName(item.getName());
+            orderDetail.setImage(item.getImage());
+            orderDetail.setAmount(item.getAmount());
+            //addAndGet 累加操作，即+=
+//            BigDecimal不能直接进行符号运算
+            amount.addAndGet(item.getAmount().multiply(new BigDecimal(item.getNumber())).intValue());
+            return orderDetail;
+        }).collect(Collectors.toList());
 
 
+        orders.setId(orderId);
+        orders.setOrderTime(LocalDateTime.now());
+        orders.setCheckoutTime(LocalDateTime.now());
+        orders.setStatus(2);
+        orders.setAmount(new BigDecimal(amount.get()));//总金额，BigDecimal可以防止浮点运算错误
+        orders.setUserId(userId);
+        orders.setNumber(String.valueOf(orderId));
+        orders.setUserName(user.getName());
+        orders.setConsignee(addressBook.getConsignee());
+        orders.setPhone(addressBook.getPhone());
+        orders.setAddress((addressBook.getProvinceName() == null ? "" : addressBook.getProvinceName())
+                + (addressBook.getCityName() == null ? "" : addressBook.getCityName())
+                + (addressBook.getDistrictName() == null ? "" : addressBook.getDistrictName())
+                + (addressBook.getDetail() == null ? "" : addressBook.getDetail()));
+
+        //向订单表插入数据，一条数据
+
+
+        this.save(orders);
+
+        //向订单明细表来插入数据，多条数据
+        orderDetailService.saveBatch(orderDetails);
+
+        //清空购物车数据
+        shoppingCartService.remove(queryWrapper);
 
     }
 }
