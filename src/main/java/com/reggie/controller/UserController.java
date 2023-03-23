9c6fd975c0df9354274e0502615961761e7e1a8a
zhu0Li <522192251@qq.com>
2023-03-23 17:51:46 +0800

    实现redis缓存验证码功能

--- src/main/java/com/reggie/controller/UserController.java
+++ src/main/java/com/reggie/controller/UserController.java
@@ -8,6 +8,7 @@ import com.reggie.entity.User;
 import com.reggie.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ import javax.mail.MessagingException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/user")
@@ -26,6 +28,8 @@ public class UserController{
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private RedisTemplate redisTemplate;
     /**
      * 发送手机短信验证码
      * @param user
@@ -42,8 +46,13 @@ public class UserController{
             //这里的phone其实就是邮箱，code是我们生成的验证码
             MailUtils.sendTestMail(phone, code);
             log.info("验证码为：{}",code);
+
             //验证码存session，方便后面拿来对比进行登录
-            session.setAttribute(phone, code);
+//            session.setAttribute(phone, code);
+
+            //将生成的验证码缓存到Redis中，并设置有效期为5分钟
+            redisTemplate.opsForValue().set(phone,code,5, TimeUnit.MINUTES);
+
             return R.success("验证码发送成功");
         }
         return R.error("验证码发送失败");
@@ -60,8 +69,12 @@ public class UserController{
         String phone = map.get("phone").toString();
         //获取验证码
         String code = map.get("code").toString();
+
         //根据手机号，从session中获取保存的验证码
-        Object codeInSession = session.getAttribute(phone).toString();
+//        Object codeInSession = session.getAttribute(phone).toString();
+
+        //从redis中获取缓存的验证码
+        Object codeInSession = redisTemplate.opsForValue().get(phone);
         //进行验证码的比对（页面提交的验证码和session中保存的验证码）
         if (codeInSession!=null&&codeInSession.equals(code)){
             //比对成功，则登陆成功
@@ -78,6 +91,10 @@ public class UserController{
             }
             //记得将当前session中的id设置为用户id，不然会继续拦截
             session.setAttribute("user",userInTab.getId());
+
+            //如果用户登录成功，删除redis中缓存的验证码
+            redisTemplate.delete(phone);
+
             return R.success(userInTab);
         }
         return R.error("登陆失败");
