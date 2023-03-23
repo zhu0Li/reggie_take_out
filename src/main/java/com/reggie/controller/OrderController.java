83b7ff9ca540cf84d2166f123970c334e7f59991
zhu0Li <522192251@qq.com>
2023-03-23 16:48:11 +0800

    准备进行项目优化

--- src/main/java/com/reggie/controller/OrderController.java
+++ src/main/java/com/reggie/controller/OrderController.java
@@ -1,6 +1,7 @@
 package com.reggie.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.reggie.common.BaseContext;
 import com.reggie.common.R;
@@ -10,11 +11,13 @@ import com.reggie.entity.Orders;
 import com.reggie.service.OrderDetailService;
 import com.reggie.service.OrderService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 /**
@@ -76,5 +79,51 @@ public class OrderController {
         //日志输出看一下
         log.info("list:{}", list);
         return R.success(ordersDtoPage);
+    }
+
+    @GetMapping("/page")
+    public R<Page> page(int page, int pageSize, Long number, String beginTime, String endTime) {
+        //获取当前id
+        Page<Orders> pageInfo = new Page<>(page, pageSize);
+        Page<OrdersDto> ordersDtoPage = new Page<>(page, pageSize);
+        //条件构造器
+        LambdaQueryWrapper<Orders> queryWrapper = new LambdaQueryWrapper<>();
+        //按时间降序排序
+        queryWrapper.orderByDesc(Orders::getOrderTime);
+        //订单号
+        queryWrapper.eq(number != null, Orders::getId, number);
+        //时间段，大于开始，小于结束
+        queryWrapper.gt(!StringUtils.isEmpty(beginTime), Orders::getOrderTime, beginTime)
+                .lt(!StringUtils.isEmpty(endTime), Orders::getOrderTime, endTime);
+        orderService.page(pageInfo, queryWrapper);
+        List<OrdersDto> list = pageInfo.getRecords().stream().map((item) -> {
+            OrdersDto ordersDto = new OrdersDto();
+            //获取orderId,然后根据这个id，去orderDetail表中查数据
+            Long orderId = item.getId();
+            LambdaQueryWrapper<OrderDetail> wrapper = new LambdaQueryWrapper<>();
+            wrapper.eq(OrderDetail::getOrderId, orderId);
+            List<OrderDetail> details = orderDetailService.list(wrapper);
+            BeanUtils.copyProperties(item, ordersDto);
+            //之后set一下属性
+            ordersDto.setOrderDetails(details);
+            return ordersDto;
+        }).collect(Collectors.toList());
+        BeanUtils.copyProperties(pageInfo, ordersDtoPage, "records");
+        ordersDtoPage.setRecords(list);
+        //日志输出看一下
+        log.info("list:{}", list);
+        return R.success(ordersDtoPage);
+    }
+
+    @PutMapping
+    public R<String> changeStatus(@RequestBody Map<String, String> map) {
+        int status = Integer.parseInt(map.get("status"));
+        Long orderId = Long.valueOf(map.get("id"));
+        log.info("修改订单状态:status={status},id={id}", status, orderId);
+        LambdaUpdateWrapper<Orders> updateWrapper = new LambdaUpdateWrapper<>();
+        updateWrapper.eq(Orders::getId, orderId);
+        updateWrapper.set(Orders::getStatus, status);
+        orderService.update(updateWrapper);
+        return R.success("订单状态修改成功");
     }
 }
